9a6fc758bb5a8c56a9847e16d1a7de156c070dfc
NayakwadiS <39816455+NayakwadiS@users.noreply.github.com>
2020-06-30 11:28:30 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+from recommonmark.transform import AutoStructify
+from recommonmark.parser import CommonMarkParser
+
+# -- Project information -----------------------------------------------------
+
+project = 'mftool'
+
+# The master toctree document.
+master_doc = 'index'
+
+html_favicon = 'mftool.png'
