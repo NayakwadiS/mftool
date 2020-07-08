126ab3dccbc5221123836ac392562260a707825b
NayakwadiS <39816455+NayakwadiS@users.noreply.github.com>
2020-07-08 18:07:13 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ project = 'mftool'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 # The master toctree document.
 master_doc = 'index'
