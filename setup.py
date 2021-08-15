edee03f268c4d38eb2632becdbf04cdb388a0bcd
Void_Man <39816455+NayakwadiS@users.noreply.github.com>
2021-08-15 12:12:36 +0530

    v2.2

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name="mftool",
     version="2.2",
-    author="Nayakwadi S",
+    author="SujitN",
     author_email="nayakwadi_sujit@rediffmail.com",
-    description="Library for extracting real time Mutual funds data in India",
+    description="Library for getting real time Mutual funds info",
     license="MIT",
     keywords="amfi, quote, mutual-funds, funds, bse, nse, market, stock, stocks",
     install_requires=['requests', 'bs4', 'httpx'],
