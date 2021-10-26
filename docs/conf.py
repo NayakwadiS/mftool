c57459f8b47b9ef686063cbe3b6c113040c14f96
Void_Man <39816455+NayakwadiS@users.noreply.github.com>
2021-10-26 17:15:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,6 @@ html_favicon = 'mftool.png'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v20210815')
+    app.add_stylesheet('css/custom.css?v20211026')
     
     
