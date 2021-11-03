f1c6aa0906e7909e3648ee9f3695af1b68e6d5e2
Void_Man <39816455+NayakwadiS@users.noreply.github.com>
2021-11-03 14:08:48 +0530

    v2.3

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ project = 'mftool'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.2'
+release = '2.3'
 
 # The master toctree document.
 master_doc = 'index'
@@ -18,6 +18,6 @@ html_favicon = 'mftool.png'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v20211026')
+    app.add_stylesheet('css/custom.css?v20211103')
     
     
