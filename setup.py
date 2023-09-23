faa2527a6f656faef23678eed08280c0bd6c1dad
Sujit Nayakwadi <sujit.nayakwadi.ext@siemens-energy.com>
2023-09-23 14:27:02 +0530

    added version 2.7

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="mftool",
-    version="2.6",
+    version="2.7",
     author="SujitN",
     author_email="nayakwadi_sujit@rediffmail.com",
     description="Library for getting real time Mutual funds info",
     license="MIT",
     keywords="amfi, quote, mutual-funds, funds, bse, nse, market, stock, stocks",
-    install_requires=['requests', 'bs4', 'httpx','pandas','yfinance'],
+    install_requires=['requests', 'bs4', 'httpx', 'pandas', 'yfinance'],
     url="https://github.com/NayakwadiS/mftool",
     packages=find_packages(),
     long_description=long_description,
