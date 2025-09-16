62b15b797d719379cff0bbc534944fe1f2017ebb
Sujit Nayakwadi <sujit.nayakwadi.ext@siemens-energy.com>
2025-09-16 11:45:16 +0530

    removed scheme_codes

--- test.py
+++
@@ -1,8 +0,0 @@
-from mftool import Mftool
-
-mf = Mftool()
-# print(mf)
-
-result = mf.compare_trend(['119597', '119598'], '1-1-2015', '29-12-2018')
-# result = mf.get_scheme_historical_nav_for_dates('119597', '1-1-2015', '29-12-2018')
-print(result)
