354b0178c2263a20662d143373aaa8f0664878a2
pythonwadi <39816455+NayakwadiS@users.noreply.github.com>
2026-04-03 13:50:59 +0530

    Fix/readthedocs configuration (#119)
    * Enable Sphinx documentation configuration
    
    * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ project = 'mftool'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.1'
+release = '3.2'
 
 # The master toctree document.
 master_doc = 'index'
@@ -18,6 +18,4 @@ html_favicon = 'mftool.png'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v20260312')
-    
-    
+    app.add_css_file('css/custom.css?v20260402')
