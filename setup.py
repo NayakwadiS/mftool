7f269e8a794ab5a42b2ee8d81b79e4b5b66d7aca
pythonwadi <39816455+NayakwadiS@users.noreply.github.com>
2026-07-06 07:50:43 +0530

    version change (#129)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="mftool",
-    version="3.1",
+    version="3.3",
     author="SujitN",
     author_email="nayakwadi_sujit@rediffmail.com",
     description="Library for getting real time Mutual funds info",
