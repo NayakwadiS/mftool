d3338b154288da9fb8b473923ae1341c6a2b6ef7
z004jm3j <sujit.nayakwadi@siemens-energy.com>
2026-09-21 12:23:33 +0530

    restructure repo v3.4

--- mftool/__init__.py
+++ mftool/__init__.py
@@ -1,7 +1,7 @@
 """
     The MIT License (MIT)
 
-    Copyright (c) 2025 Sujit Nayakwadi
+    Copyright (c) 2026 Sujit Nayakwadi
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,6 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 """
-__VERSION__='3.3'
+__VERSION__='3.4'
 from .mftool import Mftool
 
